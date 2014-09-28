0d9b68e0bf412c06d807b2362ed23769f292f023
pattwu <pattwus@gmail.com>
2014-09-28 15:59:06 +0700

    new

--- config.rb
+++ config.rb
@@ -1,11 +1,24 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/stylesheets"
-sass_dir = "assets/scss"
-images_dir = "assets/images"
-javascripts_dir = "assets/javascripts"
-url: http://pattwu.github.io/leapday-master
+css_dir = "stylesheets"
+sass_dir = "stylesheets"
+images_dir = "images"
+javascripts_dir = "javascripts"
 
-output_style = :compressed
+output_style = :expanded
+environment = :production
 
+# To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
+
 line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass stylesheets scss && rm -rf sass && mv scss sass
